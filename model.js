74f6ed6de211f3acbc14a5d43b0d8c3ec04ede6e
yrong <yrong1997@gmail.com>
2017-08-02 11:06:48 +0800

    redis-crud增强(2.5h)
    1,修复多次执行lua批量删除脚本的错误
    2,统一封装redis命令执行结果作为响应输出
    3,更新及删除不存在元素以异常形式抛出

--- model.js
+++ model.js
@@ -50,11 +50,11 @@ module.exports = function (redis, name) {
                         reject(err)
                     let findByKey = (key)=> {
                         return new Promise((resolve, reject) => {
-                            redis.get(key, (err, val) => {
+                            redis.get(key, (err, res) => {
                                 if (err)
                                     reject(err)
-                                if (val)
-                                    resolve(JSON.parse(val))
+                                if (res)
+                                    resolve(JSON.parse(res))
                             })
                         })
                     }
@@ -68,71 +68,25 @@ module.exports = function (redis, name) {
 
         deleteAll:function(){
             return new Promise((resolve, reject) => {
-                redis.eval("return redis.call('del', unpack(redis.call('keys', ARGV[1])))", 0, KEY('*'), (err, res) => {
-                    resolve({})
+                redis.eval("return redis.call('del', 'default-template',unpack(redis.call('keys', ARGV[1])))", 0, KEY('*'), (err, res) => {
+                    if(err)
+                        reject(err)
+                    else
+                        resolve(res)
                 })
             })
         },
 
-        iterate(limit) {
-            let _cursor = 0;
-            let _eof = false;
-
-            // Return the object that should be used for retrieved data
-            return {
-                get eof() {
-                    return _eof;
-                },
-
-                next: function () {
-                    // Return null if eof has been reached already
-                    if (_eof) {
-                        return Promise.resolve(null);
-                    }
-
-                    const records = [];
-                    function yieldResult(count) {
-                        return redis.scanAsync(_cursor, 'match', KEY('*'), 'count', count).then(res => {
-                            // Update the cursor for further calls
-                            _cursor = parseInt(res[0]);
-
-                            // Append the result obtained into the final records list
-                            if (res[1].length > 0) {
-                                records.push.apply(records, res[1]);
-                            }
-
-                            // Check if we have reached the end of the list or
-                            // got the maximum number of records for each request
-                            if (_cursor === 0) {
-                                _eof = true;
-                                return records;
-                            } else if (records.length === limit) {
-                                return records;
-                            } else {
-                                return yieldResult(limit - records.length);
-                            }
-                        });
-                    }
-
-                    return yieldResult(limit);
-                },
-            };
-        },
-
         get(id) {
             const key = KEY(id);
-
-            // only return if the key exists
-            return redis.existsAsync(key).then(res => {
-                if (res === 0) {
-                    // Record not found
-                    return {};
-                }
-
-                return redis.getAsync(key).then((res)=>{
-                    return JSON.parse(res)
-                });
-            });
+            return new Promise((resolve, reject) => {
+                redis.get(key,(err,res)=>{
+                    if(res)
+                        resolve(JSON.parse(res))
+                    else
+                        reject(`key ${id} does not exist`)
+                })
+            })
         },
 
         getId(obj) {
@@ -141,51 +95,32 @@ module.exports = function (redis, name) {
 
         delete(id) {
             const key = KEY(id);
-
-            // Only run if the record already exists
-            return redis.existsAsync(key).then(res => {
-                if (res === 0)
-                    return false;
-
-                // Retrieve the object being deleted
-                return redis.getAsync(key);
-            }).then((obj) => {
-                // Run the beforeDelete hook
-                return Promise.resolve(this.beforeDelete && this.beforeDelete(id, obj)).then(() => {
-                    return redis.delAsync(key).then(() => {
-                        return Promise.resolve(this.afterDelete && this.afterDelete(id, obj));
-                    });
-                });
-            }).then(() => {
-                return true;
-            });
+            return new Promise((resolve, reject) => {
+                redis.get(key,(err,res)=>{
+                    if(res){
+                        Promise.resolve(this.beforeDelete && this.beforeDelete(id, obj)).then(() => {
+                            redis.del(key,(err,res)=>resolve(res))
+                        })
+                    }else{
+                        reject(`key ${id} does not exist`)
+                    }
+                })
+            })
         },
 
         update(id, obj) {
             const key = KEY(id);
-
-            // The operation can be performed only if the record exists already
-            return redis.existsAsync(key).then(res => {
-                if (res === 0)
-                    return false;
-
-                // run the before hook
-                return Promise.resolve(this.beforeUpdate && this.beforeUpdate(id, obj)).then(() => {
-
-                    return new Promise((resolve, reject) => {
-                        redis.set(KEY(id),JSON.stringify(obj),(err,res)=>{
-                            if (res === 'OK') {
-                                // run the after hook
-                                const after = this.afterUpdate && this.afterUpdate(id, obj);
-                                return Promise.resolve(after).then(() => resolve(true))
-                            } else {
-                                reject(new Error(`Could not update object for ${name} with id ${id}.` +
-                                    ` The server said ${res}`))
-                            }
+            return new Promise((resolve, reject) => {
+                redis.get(key,(err,res)=>{
+                    if(res){
+                        Promise.resolve(this.beforeUpdate && this.beforeUpdate(id, obj)).then(() => {
+                            redis.set(KEY(id),JSON.stringify(obj),(err,res)=>resolve({res}))
                         })
-                    })
-                });
-            });
+                    }else{
+                        reject(`key ${id} does not exist`)
+                    }
+                })
+            })
         },
     };
 };
