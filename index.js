cb9536bc38ce7a7706feea9a9f04629ef736364c
yrong <yrong1997@gmail.com>
2020-03-20 20:55:45 +0800

    findByPattern&code refactor

--- index.js
+++ index.js
@@ -5,88 +5,92 @@ const ID = '__id__';
 module.exports = function (redis, name) {
     const NAME = '__' + name + '__';
 
-    const KEY = function (id) {
-        return NAME + ':' + id;
+    const KEY = function (key) {
+        return NAME + ':' + key;
     };
 
-    return {
-        insert(obj) {
-            let before;
-            try {
-                before = this.beforeInsert && this.beforeInsert(obj);
-            } catch (e) {
-                return Promise.reject(e);
-            }
+    const beforeInsert = ()=>{};
 
-            return Promise.resolve(before).then(() => {
-                return new Promise((resolve, reject) => {
-                    let id = obj.id||obj.name
-                    if(id){
+    const afterInsert = ()=>{};
+
+    const insert = async (obj)=>{
+        let before;
+        try {
+            before = beforeInsert && beforeInsert(obj);
+        } catch (e) {
+            return Promise.reject(e);
+        }
+
+        return Promise.resolve(before).then(() => {
+            return new Promise((resolve, reject) => {
+                let id = obj.id||obj.name,age = obj.maxAge
+                if(id){
+                    if(age){
+                        redis.setex(KEY(id), age, JSON.stringify(obj),(err,res)=>{
+                            if (res) {
+                                const after = afterInsert && afterInsert(obj, id);
+                                return Promise.resolve(after).then(() => {
+                                    resolve(id);
+                                });
+                            }
+                        })
+                    }else{
                         redis.set(KEY(id), JSON.stringify(obj),(err,res)=>{
                             if (res) {
-                                const after = this.afterInsert && this.afterInsert(obj, id);
+                                const after = afterInsert && afterInsert(obj, id);
                                 return Promise.resolve(after).then(() => {
                                     resolve(id);
                                 });
                             }
                         })
-                    }else{
-                        reject(new Error(`missing id or name field`))
                     }
-                })
-            });
-        },
-
-        insertEX(obj) {
-            let before;
-            try {
-                before = this.beforeInsert && this.beforeInsert(obj);
-            } catch (e) {
-                return Promise.reject(e);
-            }
-
-            return Promise.resolve(before).then(() => {
-                return new Promise((resolve, reject) => {
-                    let id = obj.id||obj.name,maxAge=obj.maxAge||1800
-                    if(id){
-                        redis.setex(KEY(id), maxAge,JSON.stringify(obj),(err,res)=>{
-                            if (res) {
-                                const after = this.afterInsert && this.afterInsert(obj, id);
-                                return Promise.resolve(after).then(() => {
-                                    resolve(id);
-                                });
-                            }
-                        })
-                    }else{
-                        reject(new Error(`missing id or name field`))
-                    }
-                })
-            });
-        },
-
-        findAll: function(){
-            return new Promise((resolve, reject) => {
-                let results = [],promises = []
-                redis.keys(KEY('*'),(err,keys)=>{
-                    if(err)
-                        reject(err)
-                    let findByKey = (key)=> {
-                        return new Promise((resolve, reject) => {
-                            redis.get(key, (err, res) => {
-                                if (err)
-                                    reject(err)
-                                if (res)
-                                    resolve(JSON.parse(res))
-                            })
-                        })
-                    }
-                    for(let key of keys){
-                        promises.push(findByKey(key))
-                    }
-                    Promise.all(promises).then(resolve).catch(reject)
-                });
+                }else{
+                    reject(new Error(`missing id or name field`))
+                }
             })
-        },
+        });
+    }
+
+    const insertEX = async (obj)=>{
+        obj.maxAge = obj.maxAge||1800
+        return insert(obj)
+    }
+
+    const findByPattern = async function(pattern){
+        return new Promise((resolve, reject) => {
+            let results = [],promises = []
+            redis.keys(KEY(pattern),(err,keys)=>{
+                if(err)
+                    reject(err)
+                let findByKey = (key)=> {
+                    return new Promise((resolve, reject) => {
+                        redis.get(key, (err, res) => {
+                            if (err)
+                                reject(err)
+                            if (res)
+                                resolve(JSON.parse(res))
+                        })
+                    })
+                }
+                for(let key of keys){
+                    promises.push(findByKey(key))
+                }
+                Promise.all(promises).then(resolve).catch(reject)
+            });
+        })
+    }
+
+    const findAll = async()=>{
+        return findByPattern('*')
+    }
+
+    return {
+        beforeInsert,
+        afterInsert,
+        insert,
+        insertEX,
+        findByPattern,
+        findAll,
 
         deleteAll:function(){
             return new Promise((resolve, reject) => {
